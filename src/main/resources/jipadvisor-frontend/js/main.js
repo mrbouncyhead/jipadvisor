0a16112167c0338fffa5a987fd4fb417d2d1823e
Mark Howard <bark4mark@gmail.com>
2017-11-21 21:19:06 +0000

    Implementing expiremental frontend

--- src/main/resources/jipadvisor-frontend/js/main.js
+++ src/main/resources/jipadvisor-frontend/js/main.js
@@ -1,6 +1,32 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
+var defaultView = {
+  template: '#default-template'
+}
+
+var loginView = {
+  template: '#login-template'
+}
+
+var registerView = {
+  template: '#register-template'
+}
+
+Vue.component('main-menu', {
+  name: 'main-menu',
+  template: '#menu-template'
 })
+
+var router = new VueRouter({
+  routes: [{
+    path: '/', component: defaultView
+  },
+  {
+    path: '/login', component: loginView
+  },
+  {
+    path: '/register', component: registerView
+  }]
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
